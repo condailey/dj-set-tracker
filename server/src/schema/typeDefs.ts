94c1c222cfa5b5979e43364e2e3b59caee2215f5
Connor Dailey <condailey1@gmail.com>
2026-03-17 14:49:37 -0500

    Add GraphQL schema with Track type, queries, and addTrack mutation

---
+++ server/src/schema/typeDefs.ts
@@ -0,0 +1,35 @@
+const typeDefs = `
+    type Track {
+        id: ID!
+        title: String!
+        artist: String!
+        bpm: Float!
+        musicalKey: String
+        genre: String
+        energyLevel: String!
+        notes: String
+    }
+
+    type Query {
+        tracks: [Track!]!
+        track(id: ID!): Track
+        tracksByGenre(genre: String!): [Track!]!
+        tracksByBpmRange(min: Float!, max: Float!): [Track!]!
+    }
+
+    input TrackInput {                                                                  
+      title: String!                                                                  
+      artist: String!
+      bpm: Float!                                                                     
+      musicalKey: String
+      genre: String                                                                   
+      energyLevel: String!
+      notes: String                                                                   
+    } 
+
+    type Mutation { 
+      addTrack(input: TrackInput!): Track!
+    }
+`;
+
+export default typeDefs;
