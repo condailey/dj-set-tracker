a00c932cab982a0211387a057c7eed0c22c79d44
Connor Dailey <condailey1@gmail.com>
2026-03-19 13:05:33 -0500

    Add Set, SetTrack, Transition types and new   mutations to GraphQL schema

--- server/src/schema/typeDefs.ts
+++ server/src/schema/typeDefs.ts
@@ -9,6 +9,30 @@ const typeDefs = `
         energyLevel: String!
         notes: String
     }
+    
+    type Set {
+        id: ID!
+        name: String!
+        datePlayed: String
+        venue: String
+        notes: String
+        status: String!
+        tracks: [SetTrack!]
+    }
+
+    type SetTrack {
+        id: ID!
+        track: Track!
+        position: Int!
+    }
+    
+    type Transition {
+        id: ID!
+        fromTrack: Track!
+        toTrack: Track!
+        rating: String!
+        notes: String
+    }
 
     type Query {
         tracks: [Track!]!
@@ -16,6 +40,8 @@ const typeDefs = `
         tracksByGenre(genre: String!): [Track!]!
         tracksByBpmRange(min: Float!, max: Float!): [Track!]!
         compatibleTracks(trackId: ID!): [Track!]!
+        sets: [Set!]!
+        set(id: ID!): Set
     }
 
     input TrackInput {                                                                  
@@ -26,10 +52,32 @@ const typeDefs = `
       genre: String                                                                   
       energyLevel: String!
       notes: String                                                                   
-    } 
+    }
+
+    input SetInput {
+        name: String!
+        datePlayed: String
+        venue: String
+        notes: String
+        status: String!
+    }
+    
+    input TransitionInput {
+        setId: ID!
+        fromTrackId: ID!
+        toTrackId: ID!
+        rating: String!
+        notes: String
+    }
 
     type Mutation { 
-      addTrack(input: TrackInput!): Track!
+        addTrack(input: TrackInput!): Track!
+        updateTrack(id: ID!, input: TrackInput!): Track!
+        deleteTrack(id: ID!): Boolean!
+        createSet(input: SetInput!): Set!
+        addTrackToSet(setId: ID!, trackId: ID!, position: Int!): SetTrack!
+        removeTrackFromSet(id: ID!): Boolean!
+        rateTransition(input: TransitionInput!): Transition!
     }
 `;
 
